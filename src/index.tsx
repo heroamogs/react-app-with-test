f9c10351accbe67365791e67202388339f5608ca
Samuel Okolo <herookolo@gmail.com>
2023-03-05 23:07:08 +0100

    feat: added pre-commit hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render( 
-  <div> This is a new react app from Samuel</div>, 
-  document.getElementById('root')
-);
+ReactDOM.render(<div> This is a new react app from Samuel</div>, document.getElementById('root'));
