2eb5df2f0695ce2164cc40640edadd28564efad5
Samuel Okolo <herookolo@gmail.com>
2023-03-12 23:07:08 +0100

    feat: modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export const App = () => <div> This is a new to do and react app from Samuel </div>;
+export const App = () => <div> This is a new to do and awesome react app from Samuel </div>;
